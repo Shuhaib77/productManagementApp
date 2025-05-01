0e50949aca0b55c9d4a0d5cad885ab8b4e9770a3
Shuhaib77 <shuhaibbasheer20022@gmail.com>
2025-05-02 03:23:56 +0530

    completed

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import AuthLayOut from "./pages/auth/AuthLayOut";
 import Register from "./pages/auth/register/Register";
 import Login from "./pages/auth/login/Login";
-import HomeLayOut from "./pages/nonauth/dashboard/HomeLayOut";
+import HomeLayOut from "./pages/nonauth/home/HomeLayOut";
 import ProdctLayOut from "./pages/nonauth/productdetails/ProdctLayOut";
 
 function UserRouter() {
