fc9170f3733aef8f1d239ffee2a4228543a84412
Shuhaib77 <shuhaibbasheer20022@gmail.com>
2025-05-02 02:59:30 +0530

    compleated

--- src/common/layout/Sidebar.jsx
+++ src/common/layout/Sidebar.jsx
@@ -4,7 +4,7 @@ import Accordion from "../components/accordion/Accordion";
 function Sidebar({ catData, getSubcatData, subCatdata }) {
   console.log(catData, "oo");
   return (
-    <div className="w-[290px] h-[85vh] shadow-md flex flex-col gap-4 items-start p-2 overflow-y-auto">
+    <div className="w-[290px] h-[83vh] shadow-md flex flex-col gap-4 items-start p-2 overflow-y-auto">
       <h2>allcatogeriess</h2>
       <h1>Catogeries</h1>
 
